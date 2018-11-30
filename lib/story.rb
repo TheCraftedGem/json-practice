7b0d5202c3e71e8615988fb13e3fd51c1c6cad7b
Emilio <thecraftedgem@tuta.io>
2018-11-30 13:01:31 -0700

    getting closer

--- lib/story.rb
+++ lib/story.rb
@@ -2,12 +2,40 @@ require 'json'
 require 'pry'
 
 class Story
-  attr_reader :data_hash, :story
+  attr_reader :data,
+              :stories,
+              :section,
+              :subsection,
+              :link,
+              :published,
+              :photo,
+              :title,
+              :abstract
 
-  def initialize(file)
-    @data_hash = JSON.parse(file)
-    @story = []
-    
+  def initialize(data)
+    @data = data
+    @stories = []
+    @section = []
+    @subsection = []
+    @title = []
+    @abstract = []
+    @link = []
+    @published = []
+    @photo = []
+  end 
+
+  def create_section
+    @section = @data["results"].map do |story|
+      Story.new(story["section"])
+    end
+  binding.pry
+    #section - String
+    #subsection - String
+    #title - String - text containing the string of the story
+    #abstract - String - summary of the story
+    #link - String - URL of where the story is located
+    #published - String - Date that the story was published. Example format: "November 29, 2019"
+    #photo
   end
 
 end
--- test/story_test.rb
+++ test/story_test.rb
@@ -6,19 +6,23 @@ require_relative '../lib/story'
 
 class StoryTest < Minitest::Test
 
+  def setup 
+    @data = JSON.parse(File.read('data/nytimes.json'))
+  end
+
   def test_it_exists 
-    story = Story.new(File.read('data/nytimes.json'))
+    story = Story.new(@data)
     assert_instance_of Story, story
   end
 
+
   def test_it_can_load_section
-    story = Story.new(File.read('data/nytimes.json'))
-  
-    section = story.data_hash["results"].map do |element|
-      story.story << element["section"]
-    end
-    binding.pry
-    assert_equal 43, story.story.count
+
+    story = Story.new(@data)
+    
+    story.create_section
+    assert_equal 1, story.section.count
   end
+  
 
 end
